408dca003119b49e382fd9d336d28f9fbe0f7e0b
satvik shukla <satvikshukla9@gmail.com>
2017-11-14 13:53:24 -0800

    a

--- exercise-1/scripts/BuildScatter.R
+++ exercise-1/scripts/BuildScatter.R
@@ -8,3 +8,10 @@
 #   - Title of the plot (set a default of "Title")
 #   - Label for the x axis (set a default of "X Title")
 #   - Label for the y axis (set a default of "Y Title")
+
+source("plotly")
+
+BuildPlot <- function(df, xpar, ypar, co, title = "Title", xlab = "X Title", ylab = "Y Title") {
+	plot <- plot_ly(data = df, x = xpar, y = ypar, name = title, color = co) %>% layout(xaxis = list(title = xlab), yaxis = list(title = ylab))
+	return plot
+}
